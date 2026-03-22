f987afe52b836d7f3d57cdacb6917ce7f0346c0f
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2026-03-22 19:07:21 +0530

    Add ResultPage to navigation stack

--- Loanapproval-app/App.js
+++ Loanapproval-app/App.js
@@ -4,6 +4,7 @@ import Login from './src/component/Login/Login';
 import Application from './src/component/application/application';
 import ExistingLoanDetails from './src/component/existingloandetails/existingloandetails';
 import DocumentUploadPage from './src/component/document-upload-page/document-upload-page';
+import ResultPage from './src/component/result-page/result-page';
 
 const Stack = createNativeStackNavigator();
 
@@ -20,6 +21,7 @@ export default function App() {
         <Stack.Screen name="Application" component={Application} />
         <Stack.Screen name="ExistingLoanDetails" component={ExistingLoanDetails} />
         <Stack.Screen name="DocumentUploadPage" component={DocumentUploadPage} />
+        <Stack.Screen name="ResultPage" component={ResultPage} />
       </Stack.Navigator>
     </NavigationContainer>
   );
